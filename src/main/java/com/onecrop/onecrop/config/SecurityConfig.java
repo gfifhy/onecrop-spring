d57930929158427b3bf88eea2bf66880507ecf7c
James <jamesga777@gmail.com>
2024-11-19 23:21:07 +0800

    disabled csrf for the mean time

--- src/main/java/com/onecrop/onecrop/config/SecurityConfig.java
+++ src/main/java/com/onecrop/onecrop/config/SecurityConfig.java
@@ -23,6 +23,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authorize) -> authorize
                         .anyRequest().permitAll()
                         //.anyRequest().authenticated()
