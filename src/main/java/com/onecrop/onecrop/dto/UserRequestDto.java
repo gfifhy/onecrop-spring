3018d0a10eeb715f0b84dbdd17d53f5526575e02
James <jamesga777@gmail.com>
2024-11-19 23:22:06 +0800

    Added User Request Dto

---
+++ src/main/java/com/onecrop/onecrop/dto/UserRequestDto.java
@@ -0,0 +1,30 @@
+package com.onecrop.onecrop.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class UserRequestDto {
+        @Email
+        @NotEmpty(message = "email is required")
+        private String email;
+
+        @NotEmpty(message = "Username is required")
+        @Size(min = 3, message = "Username length must be at least 3 characters")
+        private String username;
+
+        @NotEmpty(message = "password is required")
+        @Size(min = 8, message = "Password length must be at least 8 characters")
+        private String password;
+
+        @NotEmpty(message = "firstname is required")
+        private String firstName;
+
+        @NotEmpty(message = "lastname is required")
+        private String lastName;
+
+}
