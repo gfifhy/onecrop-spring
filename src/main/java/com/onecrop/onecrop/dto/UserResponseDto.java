d9802b69739cc62247253eb8f969690304b6f29a
James <jamesga777@gmail.com>
2024-11-19 23:22:13 +0800

    Added User Response Dto

---
+++ src/main/java/com/onecrop/onecrop/dto/UserResponseDto.java
@@ -0,0 +1,30 @@
+package com.onecrop.onecrop.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Setter
+@Getter
+public class UserResponseDto {
+
+    @NotEmpty
+    private UUID id;
+
+    @Email
+    @NotEmpty
+    private String email;
+
+    @NotEmpty
+    private String username;
+
+    @NotEmpty
+    private String firstName;
+
+    @NotEmpty
+    private String lastName;
+
+}
