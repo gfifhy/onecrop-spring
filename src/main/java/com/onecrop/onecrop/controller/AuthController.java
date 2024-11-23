61095bd57d1df7b3a59bedea49ff44f46006f5be
James <jamesga777@gmail.com>
2024-11-24 05:03:58 +0800

    Added dev profile

--- src/main/java/com/onecrop/onecrop/controller/AuthController.java
+++ src/main/java/com/onecrop/onecrop/controller/AuthController.java
@@ -3,12 +3,16 @@ package com.onecrop.onecrop.controller;
 import com.onecrop.onecrop.dto.LoginDto;
 import com.onecrop.onecrop.dto.UserRequestDto;
 import com.onecrop.onecrop.dto.UserResponseDto;
+import com.onecrop.onecrop.mapper.UserMapper;
+import com.onecrop.onecrop.model.User;
+import com.onecrop.onecrop.repository.UserRepository;
 import com.onecrop.onecrop.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -18,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 
 @RestController
@@ -26,7 +31,9 @@ import java.util.Map;
 public class AuthController {
 
     private final UserService userService;
+    private final UserRepository userRepository;
     private AuthenticationManager authenticationManager;
+    private UserMapper userMapper;
 
     @PostMapping("/register")
     public ResponseEntity<UserResponseDto> register(@Valid @RequestBody UserRequestDto user){
@@ -50,6 +57,25 @@ public class AuthController {
             throw new RuntimeException("Invalid username or password");
         }
     }
+
+
+    @GetMapping("/profile")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<UserResponseDto> profile(HttpServletRequest request) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = userRepository.findByUsername(authentication.getName()).orElseThrow();
+        return new ResponseEntity<>(userMapper.userEntitytoUserResponseDto(currentUser), HttpStatus.OK);
+    }
+
+    @PostMapping("/logout")
+    @PreAuthorize("isAuthenticated()")
+    @ResponseStatus(HttpStatus.OK)
+    public void logout(HttpServletRequest request) {
+        SecurityContextHolder.clearContext();
+        request.getSession().invalidate();
+    }
+
+
 
     @GetMapping("/debug/user")
     public Map<String, Object> getCurrentUser(HttpServletRequest request) {
